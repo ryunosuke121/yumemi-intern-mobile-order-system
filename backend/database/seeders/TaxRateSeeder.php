31483f3d3d4cfdf6ff146264351418d88fa7b05b
ryunosuke121 <ryunosuke.ito.121@gmail.com>
2024-09-06 14:06:12 +0900

    税金マスタのシーダー実装

---
+++ backend/database/seeders/TaxRateSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaxRateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $taxRates = [
+            [
+                'tax_rate' => 1.0,
+            ],
+            [
+                'tax_rate' => 1.08
+            ],
+            [
+                'tax_rate' => 1.10
+            ]
+        ];
+
+        foreach ($taxRates as $taxRate) {
+            \App\Models\MTaxRate::create($taxRate);
+        }
+    }
+}
