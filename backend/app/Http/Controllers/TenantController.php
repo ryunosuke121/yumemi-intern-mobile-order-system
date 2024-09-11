e509d17c2ee260a2277875a76c7baa0af91da47b
ryunosuke121 <ryunosuke.ito.121@gmail.com>
2024-09-11 12:09:47 +0900

    fix: パスパラメータが文字列として渡される問題を修正

--- backend/app/Http/Controllers/TenantController.php
+++ backend/app/Http/Controllers/TenantController.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Constants\MessageConst;
 use App\Http\Requests\CreateItemRequest;
 use App\Http\Requests\Tenant\ChangeTableCountRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Http\Resources\ItemResource;
+use App\Usecases\Order\Exceptions\ItemNotFoundException;
 use App\Usecases\Tenant\ChangeTableCountAction;
 use App\Usecases\Tenant\CreateItemAction;
 use App\Usecases\Tenant\DeleteItemAction;
 use App\Usecases\Tenant\UpdateItemAction;
+use Exception;
 use Illuminate\Http\Request;
 
 final class TenantController extends Controller
@@ -31,7 +34,11 @@ final class TenantController extends Controller
 
     public function updateItem(UpdateItemRequest $request, UpdateItemAction $action): ItemResource {
         $tenant = $request->user()->tenant;
-        $itemID = $request->route('id');
+        $itemID = $request->route('id') ? (int) $request->route('id') : null;
+        if ($itemID === null) {
+            throw new ItemNotFoundException(MessageConst::ITEM_NOT_FOUND);
+        }
+        
         $newItem = $request->makeItem();
         $image = $request->file('image');
 
